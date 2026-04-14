20c17c5bf66a5cf4a68112996632bb8375093af6
deepak8487 <deepak123qweasd890@gmail.com>
2026-04-14 14:56:18 +0000

    data types

---
+++ journey/datatypes.js
@@ -0,0 +1,18 @@
+//String
+let name = "Shivay"
+//Number 
+let age = 20;
+//Boolean
+let isMarried = false;
+//Undefined
+let address;
+//Null
+let phoneNumber = null;
+
+console.table([name, age, isMarried, address, phoneNumber]); 
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof address);
+console.log(typeof phoneNumber);
